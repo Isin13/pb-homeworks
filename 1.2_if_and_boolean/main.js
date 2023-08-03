78a5c259ffa9202e0f09abae3d5cac2c92277d38
Isin13 <123600501+Isin13@users.noreply.github.com>
2023-08-03 23:29:31 +0300

    add js

---
+++ 1.2_if_and_boolean/main.js
@@ -0,0 +1,25 @@
+const input = document.getElementById('name');
+const btn = document.querySelector('button')
+const userName = 'admin';
+const balance = 11;
+
+if (userName === 'admin'){
+  console.log('administrator');
+} else if (userName === '') {
+  console.log('visitor');
+}else if ((userName !== "admin" || userName === '') && balance > 5000) {
+  console.log(`вип клиент ${userName}`);
+}else if ((userName !== "admin" || userName === '') && balance > 1000) {
+  console.log(`пакупальщик ${userName}`);
+}else if ((userName !== "admin" || userName === '') && balance > 100) {
+  console.log(`нищеброд ${userName}`);
+}else{
+  console.log('пшел вон отсель')
+}
+
+
+
+
+
+
+
